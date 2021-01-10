2d1ecd828f0bd5849c3409689f9ab093ed92ddcd
futhimhlas <futhimhl2@gmail.com>
2021-01-10 13:01:33 +0000

    API data sent to console as json object

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://api-football-v1.p.rapidapi.com/v2/teams/league/2",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "4cf27efb20msh02bf2eba86b221fp1284cejsnb77ceea62379",
-		"x-rapidapi-host": "api-football-v1.p.rapidapi.com"
-	}
-};
+function getTeamData(teamDataCB){
+    const data = null;
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
 
-$.ajax(settings).done(function (response) {
-	document.getElementById("data").innerHTML= response;
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            teamDataCB(JSON.parse(this.responseText));
+        }
+    });
+
+    xhr.open("GET", "https://api-football-v1.p.rapidapi.com/v2/teams/league/2");
+    xhr.setRequestHeader("x-rapidapi-key", "4cf27efb20msh02bf2eba86b221fp1284cejsnb77ceea62379");
+    xhr.setRequestHeader("x-rapidapi-host", "api-football-v1.p.rapidapi.com");
+
+    xhr.send(data);
+}
+
+getTeamData(function(data){
+    console.log(data);
 });
+
+
+// or make a function at the bottom of the xhr request to the api called printTeamDataToConsole(data) { console.log(data);} and then pass the function to getaTeamData(printTeamDataToConsole);
+
+
